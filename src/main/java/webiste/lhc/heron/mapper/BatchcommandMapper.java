2d4a31d5564072b056e8ad0b0c1fe57fc5e02634
longhaicheng <haicheng.long@androidmov.com>
2020-09-15 09:14:11 +0800

    添加文件

---
+++ src/main/java/webiste/lhc/heron/mapper/BatchcommandMapper.java
@@ -0,0 +1,7 @@
+package webiste.lhc.heron.mapper;
+
+import webiste.lhc.heron.config.mapperconfig.BaseMapper;
+import webiste.lhc.heron.model.Batchcommand;
+
+public interface BatchcommandMapper extends BaseMapper<Batchcommand> {
+}
