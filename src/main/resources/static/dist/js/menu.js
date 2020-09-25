2058dce53558c21386ef12e6dbaabdc0305e7b83
Funny_ <582895699@qq.com>
2020-09-26 02:15:24 +0800

    修改

--- src/main/resources/static/dist/js/menu.js
+++ src/main/resources/static/dist/js/menu.js
@@ -7,8 +7,6 @@ function delMenu(id) {
         dataType: 'json',
         async: false,
         success: function (res) {
-            // console.log(res);
-            // console.log(res.code == 0);
             fail = (res.code == 0);
         }
     });
