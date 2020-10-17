120527326e8eacfce056ab1e68bf3758151691fc
Funny_ <582895699@qq.com>
2020-10-17 23:33:10 +0800

    项目启动，保存pid

--- src/bin/start.sh
+++ src/bin/start.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 #author: 582895699@qq.com
 cd ..
-# 配置自己的启动类
 man_class='webiste.lhc.heron.HeronApplication'
 home=$(pwd)
 jvm_params='-Xms512m -Xmx2g'
@@ -11,5 +10,4 @@ nohup java $jvm_params -cp $conf_dir:$lib_dir $man_class > start.log 2>&1 &
 pid=$!
 echo "Application started successfully!"
 echo 'pid: '+$pid
-cd $home/bin
-echo $pid > pid.pid
+echo $pid > $home/pid.pid
