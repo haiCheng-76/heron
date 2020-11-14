d7431996c5c660c645b3c2b696264faa728c80ea
Funny_ <582895699@qq.com>
2020-11-14 16:35:45 +0800

    修改启动脚本，调整logback配置

--- src/bin/start.sh
+++ src/bin/start.sh
@@ -1,13 +1,17 @@
-#!/bin/sh
-#author: 582895699@qq.com
-cd ..
-man_class='webiste.lhc.heron.HeronApplication'
-home=$(pwd)
-jvm_params='-Xms512m -Xmx2g'
-conf_dir=$home/conf/
-lib_dir=$home/lib/*
-nohup java $jvm_params -cp $conf_dir:$lib_dir $man_class > start.log 2>&1 &
+#!/bin/bash
+#author:582895699@qq.com
+#date:2020年11月14日15:14:27
+PARENT_DIR=$(dirname $PWD)
+MAINCLASS="webiste.lhc.heron.HeronApplication"
+JVM_OPTS="-Xms512m -Xmx1g"
+CONSOLE_FILE_NAME="console.log"
+echo "=====  开始执行脚本  ====="
+cd $PARENT_DIR
+if [ -e $CONSOLE_FILE_NAME ]; then
+  touch $CONSOLE_FILE_NAME
+fi
+nohup java $JVM_OPTS -cp $PWD/conf:$PWD/lib/* $MAINCLASS >$PWD/$CONSOLE_FILE_NAME 2>&1 &
 pid=$!
 echo "Application started successfully!"
 echo 'pid: '+$pid
-echo $pid > $home/pid.pid
+echo $pid >$PWD/pid.pid
